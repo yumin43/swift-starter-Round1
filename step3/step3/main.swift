9d5f2d440935d4bad274236926cf4776a806cf2e
dsungc1111 <eotjd9808@naver.com>
2024-03-29 20:36:43 +0900

    changed step3

--- step3/step3/main.swift
+++ step3/step3/main.swift
@@ -12,13 +12,11 @@ import Foundation
 
 func chocoSnack(lengthOfChoco: Int, shapeOfbody: String, toping: String, lengthOfBar: Int) {
     for _ in 1...lengthOfChoco {
-        print(toping, terminator: "")
-        print(shapeOfbody, terminator: "")
-        print(toping)
+        print(" ", toping, shapeOfbody, toping, separator: "")
     }
     for _ in 1...lengthOfBar {
         print("  | |")
     }
 }
 
-chocoSnack(lengthOfChoco: 4, shapeOfbody: " ***", toping: " ", lengthOfBar: 4)
+chocoSnack(lengthOfChoco: 4, shapeOfbody: "|0|", toping: "&", lengthOfBar: 2)
