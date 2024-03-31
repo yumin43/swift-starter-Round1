eddd2127efcc1d007044db23221472b44422f5dd
ssjooon <sjoon97@naver.com>
2024-03-31 18:56:50 +0900

    rollback

--- CodeStarterCamp_Week1/main.swift
+++ CodeStarterCamp_Week1/main.swift
@@ -8,9 +8,4 @@
 
 import Foundation
 
-makeIceCream(creamSize: 11, stickSize: 4)
-
-makePepero(peperoSize: 10, pepero: "***", stickLength: 4)
-makePepero(peperoSize: 12, pepero: "***", toping: "&", stickLength: 4)
-makePepero(peperoSize: 12, pepero: "***", toping: "#", stickLength: 6)
-makePepero(peperoSize: 6, pepero: "|0|", stickLength: 4)
+print("hello world")
