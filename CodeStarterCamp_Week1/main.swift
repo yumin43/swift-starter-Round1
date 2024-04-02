dcd9a42287ecafb31eca3b068d493c641ad7bf47
ssjooon <sjoon97@naver.com>
2024-04-02 23:37:58 +0900

    [Round1] Step3 - 피드백 반영 - 토핑 단어 수정 - 빼빼로 함수 분리

--- CodeStarterCamp_Week1/main.swift
+++ CodeStarterCamp_Week1/main.swift
@@ -1,6 +1,6 @@
 makeIceCream(creamSize: 11, stickSize: 4)
 
 makePepero(peperoSize: 10, pepero: "***", stickLength: 4)
-makePepero(peperoSize: 12, pepero: "***", toping: "&", stickLength: 4)
-makePepero(peperoSize: 12, pepero: "***", toping: "#", stickLength: 6)
+makePepero(peperoSize: 12, pepero: "***", topping: "&", stickLength: 4)
+makePepero(peperoSize: 12, pepero: "***", topping: "#", stickLength: 6)
 makePepero(peperoSize: 6, pepero: "|0|", stickLength: 4)
