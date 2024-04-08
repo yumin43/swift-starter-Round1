c4d5a011f5ae76895c992eea30458fefd4e8b91d
Hyomin Shin <hyomins013@gmail.com>
2024-04-08 15:18:14 +0900

    리뷰 코드수정

--- CodeStarterCamp_Week1/main.swift
+++ CodeStarterCamp_Week1/main.swift
@@ -1,24 +1,25 @@
-func printInfomation(totalLength length : Int, peperoBody body : String, peperoTopping topping : String, peperoHandle handle : Int) {
-    print("길이 : \(length)")
-    print("몸통 : \(body)")
-    print("토핑 : \(topping)")
-    print("막대길이 : \(handle)")
-    print("")
+func orderPepero(totalLength length : Int, peperoBody body : String, peperoTopping topping : String, peperoHandle handle : Int) {
+    print("길이: \(length)\n몸통: \(body)\n토핑: \(topping)\n막대길이: \(handle)\n")
+    makePepero(totalLength: length, peperoBody: body, peperoTopping: topping, peperoHandle: handle)
 }
 
-func drawPepero(totalLength length : Int, peperoBody body : String, peperoTopping topping : String, peperoHandle handle : Int) {
+func makePepero(totalLength length : Int, peperoBody body : String, peperoTopping topping : String, peperoHandle handle : Int) {
+    let totalChars = topping.count * 2 + body.count
+    
     for _ in 1...length {
         print(" ", topping, body, topping, separator: "")
     }
     for _ in 1...handle {
-        if (topping == "") {
+        for _ in 1...(totalChars / 2) {
+            print(" ", terminator: "")
+        }
+        if totalChars % 2 == 0 {
             print(" | |")
         } else {
-            print("  | |")
+            print("| |")
         }
     }
 }
 
-printInfomation(totalLength: 12, peperoBody: "|0|", peperoTopping: "", peperoHandle: 6)
-drawPepero(totalLength: 12, peperoBody: "|0|", peperoTopping: "", peperoHandle: 6)
+orderPepero(totalLength: 8, peperoBody: "|0|", peperoTopping: "***", peperoHandle: 4)
 
