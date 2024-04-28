f293f0dcbe24c5acfc28c6939add2f75fde46b26
SeBin-Kwon <sbkwon98@gmail.com>
2024-04-28 17:53:07 +0900

    Add makePepero function

--- CodeStarterCamp_Week1/Step3Pepero.swift
+++ CodeStarterCamp_Week1/Step3Pepero.swift
@@ -6,3 +6,37 @@
 //
 
 import Foundation
+
+
+func drawBodyWithTopping(body: String, topping: String) -> String {
+    return topping + body + topping
+}
+
+func drawBody(body: String, length: Int) {
+    for _ in 0..<length {
+        print(body)
+    }
+}
+
+func drawStick(body: String, length: Int) {
+    let spaceCount = max((body.count-3)/2, 0)
+    let space = String(repeating: " ", count: spaceCount)
+    let stick: String
+    if body.count % 2 == 0 && body.count > 3 {
+        stick = space + "|  |" + space
+    } else if body.count < 3 {
+        stick = space + "|" + space
+    } else {
+        stick = space + "| |" + space
+    }
+    for _ in 0..<length {
+        print(stick)
+    }
+}
+
+func makePepero(bodyLength: Int, body: String, stickLength: Int, topping: String = " ") {
+    print("<정보>\n길이: \(bodyLength)\n몸통: \(body)\n토핑: \(topping)\n막대길이: \(stickLength)\n")
+    let bodyWithTopping = drawBodyWithTopping(body: body, topping: topping)
+    drawBody(body: bodyWithTopping, length: bodyLength)
+    drawStick(body: bodyWithTopping, length: stickLength)
+}
