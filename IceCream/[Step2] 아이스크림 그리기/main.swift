c0602fb8960074c97a381bebca5d0f4347a751db
peat <ldc1000@naver.com>
2024-04-29 20:18:49 +0900

    아이스크림 그리기

--- IceCream/[Step2] 아이스크림 그리기/main.swift
+++ IceCream/[Step2] 아이스크림 그리기/main.swift
@@ -26,3 +26,41 @@ func printWholeIceCream(iceCream: Int, stick: Int) {
 }
 
 printWholeIceCream(iceCream: 8, stick: 4)
+
+//
+//  main.swift
+//  CodeStarterCamp_Week1
+//
+//  Created by yagom.
+//  Copyright © yagom academy. All rights reserved.
+//
+print("\n\n\n\n\n\n\n")
+import Foundation
+
+func drawIceCream(height: Int, width: Int) {
+    let iceCreamString = String(repeating: "*", count: width)
+    for _ in 0..<height {
+        print(iceCreamString)
+    }
+}
+
+func drawStick(height: Int, width: Int) {
+    let space = (width - 3) / 2
+    let spaceString = String(repeating: " ", count: space)
+    var stickString = ""
+    if width % 2 == 0 && width > 3 {
+        stickString = spaceString + "|  |" + spaceString
+    } else {
+        stickString = spaceString + "| |" + spaceString
+    }
+    for _ in 0..<height {
+        print(stickString)
+    }
+}
+
+func makeIceCream(iceCreamHeight: Int, stickHeight: Int, iceCreamWidth: Int) {
+    drawIceCream(height: iceCreamHeight, width: iceCreamWidth)
+    drawStick(height: stickHeight, width: iceCreamWidth)
+}
+
+makeIceCream(iceCreamHeight: 9, stickHeight: 4, iceCreamWidth: 50)
