d268ecdef326d33d48079663d7697be807cb4f19
kingkiboots <tuffhr4@gmail.com>
2024-05-30 00:34:34 +0900

    ADD) 아이스크림 그리는 함수 추가 및 실행

---
+++ CodeStarterCamp_Week1/Step2/IcecreamPainter.swift
@@ -0,0 +1,23 @@
+//
+//  IcecreamPainter.swift
+//  CodeStarterCamp_Week1
+//
+//  Created by 김기현 on 5/30/24.
+//
+
+import Foundation
+
+
+func paintIcecreamBody(height: Int){
+    for _ in 1...height {
+        print("***********")
+    }
+}
+
+func printIcecreamBar(height: Int){
+    for _ in 1...height {
+        print("    | |")
+    }
+}
+
+
