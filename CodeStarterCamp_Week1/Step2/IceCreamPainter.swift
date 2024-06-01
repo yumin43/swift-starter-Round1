395a53dd020280848de57adcb2a62ffcd9243e88
kingkiboots <tuffhr4@gmail.com>
2024-06-01 17:04:13 +0900

    FIX) 피드백 내용 주석 추가 및 String(repeating:cout:) 적용

--- CodeStarterCamp_Week1/Step2/IceCreamPainter.swift
+++ CodeStarterCamp_Week1/Step2/IceCreamPainter.swift
@@ -7,15 +7,23 @@
 
 import Foundation
 
+
+/*
+    FEEDBACK1 (20240531)
+        - 한 문자열을 반복해서 넣기(자바에서의 "*" * 11) String(repeating: "*", count: 11)
+                참고 : https://developer.apple.com/documentation/swift/string/init(repeating:count:)-23xjt
+        - 접근제한자는 객체지향의 은닉화와 캡슐화에 대한 부분이기 때문에 여기서는 중요하지 않음
+ */
+
 /**
  콘솔에 아이스크림의 몸통을 출력하는 함수
  
  - parameters:
  - height: 아이스크림 몸통의 높이("\*\*\*\*\*\*\*\*\*\*\*" 이 몇 번 반복되어 그려질 것인가)
  */
-private func paintIceCreamBody(height: Int){
+func paintIceCreamBody(height: Int){
     for _ in 1...height {
-        print("***********")
+        print(String(repeating: "*", count: 11))
     }
 }
 
@@ -25,7 +33,7 @@ private func paintIceCreamBody(height: Int){
  - parameters:
     - height: 아이스크림 막대기의 높이("    | |" 이 몇 번 반복되어 그려질 것인가)
  */
-private func paintIceCreamBar(height: Int){
+func paintIceCreamBar(height: Int){
     for _ in 1...height {
         print("    | |")
     }
