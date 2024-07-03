62833dddc08915028faf733bd1149ce211ba5171
Crois0509 <crois0509@naver.com>
2024-07-03 17:59:25 +0900

    [Round1 Step 3] - Crois

--- CodeStarterCamp_Week1/main.swift
+++ CodeStarterCamp_Week1/main.swift
@@ -1,8 +1,24 @@
-//
-//  Step 3.swift
-//  CodeStarterCamp_Week1
-//
-//  Created by 장상경 on 7/3/24.
-//
+func printPoky(pokyBody: Int, pokyStick: Int, pokyBodyType: String, pokyTopping: Character) -> Void {
+    for _ in 1...pokyBody {
+        print("\(pokyTopping)\(pokyBodyType)\(pokyTopping)")
+    }
+    for _ in 1...pokyStick {
+        print(" | |")
+    }
+}
+var pokyBody: Int = 10
+var pokyStick: Int = 6
+var pokyBodyType: String = "| |"
+var pokyTopping: Character = "+"
+// 출력할 포키의 정보 입력
 
-import Foundation
+print("""
+<정보>
+길이: \(pokyBody)
+몸통: \(pokyBodyType)
+토핑: \(pokyTopping)
+막대길이: \(pokyStick)
+
+""")
+
+printPoky(pokyBody: pokyBody, pokyStick: pokyStick, pokyBodyType: pokyBodyType, pokyTopping: pokyTopping)
