81ae1ac4bd24756d6b100b1c7a548d5b5890e37f
ollekil <ollekil@kakao.com>
2024-07-11 04:26:01 +0900

    step2 아이스크림 그리기

--- CodeStarterCamp_Week1/main.swift
+++ CodeStarterCamp_Week1/main.swift
@@ -10,3 +10,28 @@ import Foundation
 
 print("Hello, World!")
 
+//MARK: - Step 2: 아이스크림 그리기
+
+//MARK: 아이스크림 부분을 그리는 함수
+func drawIceCream() {
+    for _ in 1...8 {
+        print("***********")
+    }
+}
+
+//MARK: 막대기(손잡이) 부분을 그리는 함수
+func drawStick() {
+    for _ in 1...4 {
+        print("    | |")
+    }
+}
+
+//MARK: 전체 아이스크림을 그리는 함수
+func drawIceCreamWithStick() {
+    drawIceCream()
+    drawStick()
+}
+
+drawIceCreamWithStick()
+
+
