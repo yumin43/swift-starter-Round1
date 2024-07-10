2983063e792fe26f7f93e272ae56a09bd6cb833a
ollekil <ollekil@kakao.com>
2024-07-11 04:53:13 +0900

    매개변수 추가

--- CodeStarterCamp_Week1/main.swift
+++ CodeStarterCamp_Week1/main.swift
@@ -13,23 +13,23 @@ print("Hello, World!")
 //MARK: - Step 2: 아이스크림 그리기
 
 //MARK: 아이스크림 부분을 그리는 함수
-func drawIceCream() {
-    for _ in 1...8 {
+func drawIceCream(count: Int) {
+    for _ in 1...count {
         print("***********")
     }
 }
 
 //MARK: 막대기(손잡이) 부분을 그리는 함수
-func drawStick() {
-    for _ in 1...4 {
+func drawStick(count: Int) {
+    for _ in 1...count {
         print("    | |")
     }
 }
 
 //MARK: 전체 아이스크림을 그리는 함수
 func drawIceCreamWithStick() {
-    drawIceCream()
-    drawStick()
+    drawIceCream(count: 8)
+    drawStick(count: 4)
 }
 
 drawIceCreamWithStick()
